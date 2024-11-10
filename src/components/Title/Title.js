2880da211dbf5c31d769137a1c98c70cd38c36a8
Jack Gable <81646353+jack-gable@users.noreply.github.com>
2024-11-09 17:36:14 -0800

    edited title animation

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,31 +1,20 @@
 "use client";
 
 import React, { useRef } from "react";
-import { useInView, useReducedMotion } from "framer-motion";
+import { useInView } from "framer-motion";
 import styles from "./Title.module.css";
 
 const Title = ({ heading = "h1", title }) => {
   const Tag = heading;
   const ref = useRef(null);
-  const shouldReduceMotion = useReducedMotion();
   const isInView = useInView(ref, { amount: 0.5, once: true });
-
-  const animations = shouldReduceMotion
-    ? {
-        initial: { opacity: 0 },
-        animate: { opacity: 1 },
-      }
-    : {
-        initial: { opacity: 0, y: -100 },
-        animate: { opacity: 1, y: 0 },
-      };
 
   return (
     <Tag
       ref={ref}
-      {...animations}
-      animate={isInView ? "animate" : "initial"}
       style={{
+        opacity: isInView ? 1 : 0,
+        transform: isInView ? "translateX(0)" : "translateX(-100px)",
         transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
       }}
       className={styles.title}
